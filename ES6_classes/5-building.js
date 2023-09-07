cda03c4c108ef143f7f1c0058c22d4be86ad015f
root <tayler.coon@gmail.com>
2023-09-07 21:35:53 +0000

    task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    } else if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    } else {
+      this._sqft = newSqft;
+    }
+  }
+}
