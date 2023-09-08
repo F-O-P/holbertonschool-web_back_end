70bc34ea5bf523672065823d98465e712e144f31
root <tayler.coon@gmail.com>
2023-09-08 15:34:46 +0000

    removed type error guards and fixed clone function

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,20 +1,11 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw TypeError('brand must be a string');
-    } else if (typeof motor !== 'string') {
-      throw TypeError('motor must be a string');
-    } else if (typeof color !== 'string') {
-      throw TypeError('color must be a string');
-    } else {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    const newCar = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
-    return newCar;
+    return new this.constructor();
   }
 }
