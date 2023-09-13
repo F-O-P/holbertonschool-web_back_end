27d1f1a1a42fa6435abfd26e9b4cad9e654ea210
root <tayler.coon@gmail.com>
2023-09-13 21:06:48 +0000

    added if guard for empty strings

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
   const array = [...set];
   const filteredArray = array.filter((item) => item.startsWith(startString));
   const result = filteredArray.map(item => item.slice(startString.length)).join('-')
