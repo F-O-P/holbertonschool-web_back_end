e6ce96b6d09a5eed4e64ced8a9db58a341076487
root <tayler.coon@gmail.com>
2023-09-13 13:32:39 +0000

    moved .toString to const instead of return

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
 export default function cleanSet(set, startString) {
   const array = [...set];
   const filteredArray = array.filter((item) => item.startsWith(startString));
-  const result = filteredArray.map(item => item.slice(startString.length)).join('-');
-  return result.toString();
+  const result = filteredArray.map(item => item.slice(startString.length)).join('-')
+    .toString();
+  return result;
 }
