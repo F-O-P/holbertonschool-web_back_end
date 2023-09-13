bddb609917be60946941a5c9875a478cb48621e5
root <tayler.coon@gmail.com>
2023-09-13 04:11:27 +0000

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,5 +2,5 @@ export default function cleanSet(set, startString) {
   const array = [...set];
   const filteredArray = array.filter((item) => item.startsWith(startString));
   const result = filteredArray.map(item => item.slice(startString.length)).join('-');
-  return result;
+  return result.toString();
 }
