c869f447a628adaea98182b260d253c40249476c
F-O-P <tayler.coon@gmail.com>
2023-09-15 22:18:48 +0000

    task 6 complete

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      console.log(values[0].status);
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = [];
+
+  try {
+    const userResult = await signUpUser(firstName, lastName);
+    results.push({ status: 'fulfilled', value: userResult });
+  } catch (error) {
+    results.push({ status: 'rejected', value: `Error: ${error.message}` });
+  }
+
+  try {
+    const photoResult = await uploadPhoto(fileName);
+    results.push({ status: 'fulfilled', value: photoResult });
+  } catch (error) {
+    results.push({status: 'rejected', value: `Error: ${error.message}`});
+  }
+
+  return results;
 }
