c199ca65965688c0b60f2bf487fb2293c7193328
F-O-P <tayler.coon@gmail.com>
2023-09-15 23:31:52 +0000

    task 8

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    return new Error('Division by zero');
-  } else {
-    return (numerator / denominator);
+    throw new Error('cannot divide by 0');
   }
+  return (numerator / denominator);
 }
