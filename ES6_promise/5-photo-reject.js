ecee2f02a043afdc605f3a6b969a4c39de9b03ce
F-O-P <tayler.coon@gmail.com>
2023-09-15 18:29:27 +0000

    fixed error message and linter

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
 export default function uploadPhoto(fileName) {
-  return new Promise((resolve, reject) => {
-    return reject(new Error(`${fileName} can't be uploaded`));
-  });
+  return new Promise((resolve, reject) => reject(new Error(`${fileName} can't be processed`)));
 }
